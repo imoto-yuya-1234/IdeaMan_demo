5142c411fd6216fe5ac4cb9166881b7e5e889c0a
yuya <imoto.yuya.1234@gmail.com>
2017-08-19 00:07:15 +0900

    FloatingActionButtonを追加

--- app/src/main/java/com/example/yuya/ideaman_demo/MainActivity.kt
+++ app/src/main/java/com/example/yuya/ideaman_demo/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.yuya.ideaman_demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val AddListButton: FloatingActionButton? = findViewById(R.id.addbutton)
+        AddListButton?.setOnClickListener {view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
     }
 }
